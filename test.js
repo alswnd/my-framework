b1e6186f3aff308278fec886d870e2e30fdd35b6
alswnd <alswnd0427@gmail.com>
2020-09-26 18:06:04 +0900

    Delete some codes in test

--- test.js
+++ test.js
@@ -9,19 +9,4 @@ describe("pow", () => {
 		assert.equal(pow(2, 3), 8);
 	});
 
-	it("3**4 == 81", () => {
-		assert.equal(pow(3, 4), 81);
-	});
-
-	describe("x pow 3", () => {
-		const makeTest = (x) => {
-			let expected = x * x * x;
-
-			it(`${x}**3 == ${expected}`, () => {
-				assert.equal(pow(x, 3), expected);
-			});
-		};
-
-		for (let x = 1; x <= 5; ++x) makeTest(x);
-	});
 });
