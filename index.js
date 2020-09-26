b19ad4bf876a10ef770485feefe551e218e6fbab
alswnd <alswnd0427@gmail.com>
2020-09-26 18:42:57 +0900

    Shadow DOM

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 setInterval(() => time.setAttribute("datetime", new Date()), 1000);
+
+customElements.define(
+	"show-hello",
+	class extends HTMLElement {
+		connectedCallback() {
+			const shadow = this.attachShadow({ mode: "open" });
+
+			shadow.innerHTML = `
+      <style>p { font-weight: bold; }</style>
+      <p>Hello, ${this.getAttribute("name")}</p>
+      `;
+		}
+	}
+);
