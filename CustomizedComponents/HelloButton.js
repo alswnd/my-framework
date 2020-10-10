265fbacb2c760250a2ddb4d23931aa916376eb31
alswnd <alswnd0427@gmail.com>
2020-10-10 17:09:32 +0900

    Delete unused files

--- CustomizedComponents/HelloButton.js
+++
@@ -1,8 +0,0 @@
-class HelloButton extends HTMLButtonElement {
-	constructor() {
-		super();
-		this.addEventListener("click", () => console.log("button clicked!"));
-	}
-}
-
-customElements.define("hello-button", HelloButton, { extends: "button" });
