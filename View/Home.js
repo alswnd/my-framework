70f6af9779d76b840c8690ac91a7014e6d72ce5e
alswnd <alswnd0427@gmail.com>
2020-10-21 22:59:10 +0900

    Class Component sample

--- View/Home.js
+++ View/Home.js
@@ -1,7 +1,35 @@
-const Home = {
-	render: () => {
-		return `<h1>Home<h1>`;
-	},
-};
+class Home extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	static get observedAttributes() {
+		// 모니터링 할 속성 이름
+	}
+
+	connectedCallback() {
+		// DOM에 추가되면 실행되는 method;
+		this.innerText = "hi!";
+		this.set();
+	}
+
+	disconnectedCallback() {
+		// DOM에서 제거면 실행되는 method
+	}
+
+	attributeChangedCallback(attrName, oldVal, newVal) {
+		// 속성이 추가/제거/변경되면 실행되는 method
+	}
+
+	adoptedCallback(oldDoc, newDoc) {
+		// 다른 Document에서 옮겨지면 실행되는 method
+	}
+
+	set() {
+		setTimeout(() => {
+			this.innerText = "my name is indio";
+		}, 1000);
+	}
+}
 
 export default Home;
